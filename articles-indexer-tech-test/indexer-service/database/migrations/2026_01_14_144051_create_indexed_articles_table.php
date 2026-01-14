9442a5cdfad96a2ee3898db4ca114eb9490c53af
Jonathan_Hill <jonathanrhill78@gmail.com>
2026-01-14 14:42:11 +0000

    Indexer Service migrations

---
+++ articles-indexer-tech-test/indexer-service/database/migrations/2026_01_14_144051_create_indexed_articles_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('indexed_articles', function (Blueprint $table) {
+            $table->uuid('article_id')->primary();
+            $table->string('title')->nullable();
+            $table->text('body')->nullable();
+            $table->string('status')->nullable();
+            $table->string('last_event');
+            $table->dateTime('last_event_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('indexed_articles');
+    }
+};
---
+++ articles-indexer-tech-test/indexer-service/database/migrations/2026_01_14_144056_create_processed_events_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('processed_events', function (Blueprint $table) {
+            $table->uuid('event_id')->primary();
+            $table->string('event_name');
+            $table->dateTime('processed_at');
+            $table->timestamp('created_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('processed_events');
+    }
+};
